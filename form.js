c65942a208d441c676de7b6fd1cc9bf18aa0c2b2
Cesar1357 <85760661+Cesar1357@users.noreply.github.com>
2021-09-22 10:23:28 -0500

    Update form.js

--- form.js
+++ form.js
@@ -14,14 +14,14 @@ class Form{
     }
     display() {
         this.title.html("FRUIT CATCHER");
-        this.title.position(350, 50);
+        this.title.position(360, 50);
         this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
-        this.input.position(300,400);
+        this.input.position(500,400);
         this.input.style('width', '200px');
         this.input.style('height', '20px');
         this.input.style('background', 'lavender');
-        this.button.position(300,500);
+        this.button.position(500,500);
         this.button.style('width', '200px');
         this.button.style('height', '40px');
         this.button.style('background', 'lightpink');
@@ -39,7 +39,7 @@ class Form{
             player.update();
             player.updateCount(playerCount);
             this.greeting.html("Hello " + player.name)
-            this.greeting.position(600,250);
+            this.greeting.position(500,250);
             this.greeting.style('color', 'white');
             this.greeting.style('font-size', '100px');
         });
